d7693df416746973adf5eb1a82387389c8a4b0ef
Nathan Tran <nathantt@usc.edu>
2025-04-23 04:52:38 +0000

    added some outlines

--- wordle.cpp
+++ wordle.cpp
@@ -22,6 +22,9 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    //Check for any correct placed characters - if found, create a set and look for words with the correct character in that specific position
+        //Afterwards, look at the given floating characters, look through set to find words that has the following characters in the word and put into a set   
+    //Return set of words.
 
 }
 
